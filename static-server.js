aabe3c8c03eb43353f421eb318c53a0a21eae6e2
Rehman-942 <rehmanafzal942@gmail.com>
2026-01-11 20:14:39 +0000

    updated run command

---
+++ static-server.js
@@ -0,0 +1,63 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+const port = process.env.PORT || 8080;
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+  '.wav': 'audio/wav',
+  '.mp4': 'video/mp4',
+  '.woff': 'application/font-woff',
+  '.woff2': 'application/font-woff2',
+  '.ttf': 'application/font-ttf',
+  '.eot': 'application/vnd.ms-fontobject',
+  '.otf': 'application/font-otf',
+  '.wasm': 'application/wasm'
+};
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url);
+  let pathname = parsedUrl.pathname;
+  
+  // Serve React app - always return index.html for SPA routing
+  if (pathname === '/') {
+    pathname = '/index.html';
+  }
+  
+  const filePath = path.join(__dirname, 'build', pathname);
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      // If file not found, serve index.html for SPA routing
+      fs.readFile(path.join(__dirname, 'build', 'index.html'), (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end('404 Not Found');
+          return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      });
+      return;
+    }
+    
+    const ext = path.parse(filePath).ext;
+    const contentType = mimeTypes[ext] || 'application/octet-stream';
+    
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
